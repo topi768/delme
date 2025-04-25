7c282960d1b864b9dab7f04c05c71cde6276f0bd
topi768 <topi768@inbox.ru>
2025-04-25 19:27:47 +1000

    yandex map

---
+++ components/Map.vue
@@ -0,0 +1,39 @@
+<template>
+  <client-only>
+    <div id="ymap" style="width: 100%; height: 450px"></div>
+  </client-only>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+const initYandexMap = () => {
+  ymaps.ready(() => {
+    const map = new ymaps.Map('ymap', {
+      center: [43.149169, 131.951538],
+      zoom: 11,
+      controls: [],
+    });
+
+    const customPlacemark = new ymaps.Placemark(
+      [43.149169, 131.951538],
+      {},
+      {
+        iconLayout: 'default#image',
+        iconImageHref: '/img/icons/locationMark.svg',
+        iconImageSize: [43, 32],
+        iconImageOffset: [-16, -32],
+      }
+    );
+
+    map.geoObjects.add(customPlacemark);
+  });
+};
+
+onMounted(() => {
+  const script = document.createElement('script');
+  script.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
+  script.onload = initYandexMap;
+  document.head.appendChild(script);
+});
+</script>
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -8,14 +8,7 @@
         <UIContactsList />
       </div>
       <div class="contacts__position">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11041.275630372087!2d39.82322373001738!3d46.22400244514652!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e4cbffce621131%3A0x7f5fc17c1211a2c6!2z0JLQtdGB0LXQu9Cw0Y8g0JbQuNC30L3RjCwg0JrRgNCw0YHQvdC-0LTQsNGA0YHQutC40Lkg0LrRgNCw0LksINCg0L7RgdGB0LjRjywgMzUyMDU4!5e0!3m2!1sru!2s!4v1744989280966!5m2!1sru!2s"
-          width="450"
-          height="450"
-          style="border: 0"
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <Map></Map>
       </div>
     </div>
   </div>
